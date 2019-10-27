a4079b9f5479ec65a4ea491a14987b2f5fe14688
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-27 13:46:56 +0100

    1 - tentativo fallito di replace (',','.')

--- 1/myscript.js
+++ 1/myscript.js
@@ -1,10 +1,16 @@
 // L’utente inserisce due numeri in successione, con due prompt. Il software stampa il maggiore.
 
+
+
 var num1 = parseFloat(prompt('Scrivi un numero'));
-console.log('num1 = ' + num1)
+console.log('num1 = ' + num1);
+
+// num1 = num1.replace(',','.');
 
 var num2 = parseFloat(prompt('Scrivi un altro numero'));
-console.log('num2 = ' + num2)
+console.log('num2 = ' + num2);
+
+// num2 = num2.replace(',','.');
 
 if(num1 > num2){
   console.log('Il numero maggiore è: ' + num1)
