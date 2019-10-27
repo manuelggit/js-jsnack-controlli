7cbd08c54b455eca271e4a072ff825e5e56ddbcb
Manuel Guerrini <man.guerrini@gmail.com>
2019-10-27 14:16:53 +0100

    3 - Trasformo il ciclo for in ciclo while

--- 3/myscript.js
+++ 3/myscript.js
@@ -2,11 +2,20 @@
 
 var somma = 0;
 
-for (i = 0; i < 5; i++) {
-  var numero = parseInt(prompt('inserisci un numero'));
-  console.log(numero);
-  somma = somma + numero; // somma += numero
-  console.log('aggregato: ' + somma);
+// for (i = 0; i < 5; i++) {
+//   var numero = parseInt(prompt('inserisci un numero'));
+//   console.log(numero);
+//   somma = somma + numero; // somma += numero
+//   console.log('aggregato: ' + somma);
+// }
+
+i = 0;
+while(i < 5) {
+    var numero = parseInt(prompt('inserisci un numero'));
+    console.log(numero);
+    somma = somma + numero; // somma += numero
+    console.log('aggregato: ' + somma);
+  i++;
 }
 
 console.log(somma);
